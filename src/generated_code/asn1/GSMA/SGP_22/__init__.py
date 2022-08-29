b24609b07a72e250210cba6e2b0c3088be051421
LAPTOP-AO84OQO2\impra <impratikjaiswal@gmail.com>
2022-08-29 22:45:10 +0530

    eUICC_Profile_Package 3.2 and v2_4_sgp23_1_11 added.

--- src/generated_code/asn1/GSMA/SGP_22/__init__.py
+++ src/generated_code/asn1/GSMA/SGP_22/__init__.py
@@ -8,4 +8,5 @@
 # from src.generated_code.asn1.GSMA.SGP_22.v2_2_1.sgp22 import RSPDefinitions
 # from src.generated_code.asn1.GSMA.SGP_22.v2_2_2.sgp22 import RSPDefinitions
 # from src.generated_code.asn1.GSMA.SGP_22.v2_3.sgp22 import RSPDefinitions
-from src.generated_code.asn1.GSMA.SGP_22.v2_4.sgp22 import RSPDefinitions
+# from src.generated_code.asn1.GSMA.SGP_22.v2_4.sgp22 import RSPDefinitions
+from src.generated_code.asn1.GSMA.SGP_22.v2_4_sgp23_1_11.sgp22 import RSPDefinitions
--- src/generated_code/asn1/TCA/eUICC_Profile_Package/__init__.py
+++ src/generated_code/asn1/TCA/eUICC_Profile_Package/__init__.py
@@ -6,4 +6,5 @@
 # from src.generated_code.asn1.TCA.eUICC_Profile_Package.v2_3.epp import PEDefinitions
 # from src.generated_code.asn1.TCA.eUICC_Profile_Package.v2_3_1.epp import PEDefinitions
 # from src.generated_code.asn1.TCA.eUICC_Profile_Package.v3_0.epp import PEDefinitions
-from src.generated_code.asn1.TCA.eUICC_Profile_Package.v3_1.epp import PEDefinitions
+# from src.generated_code.asn1.TCA.eUICC_Profile_Package.v3_1.epp import PEDefinitions
+from src.generated_code.asn1.TCA.eUICC_Profile_Package.v3_2.epp import PEDefinitions
