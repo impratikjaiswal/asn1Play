a1827a9efd3154d1cb8fa24abf473a4a48c7a039
LAPTOP-AO84OQO2\impra <impratikjaiswal@gmail.com>
2023-07-04 00:38:18 +0530

    logs added

---
+++ data/copy_src_files_list.bat
@@ -0,0 +1,9 @@
+@ECHO ON
+
+SET FileList=src_files_list.txt
+SET Source=E:\Backup\D\Other\Github_Self\asn1play\data
+SET Destination=D:\Other\Github_Self\asn1play\data
+
+FOR /F "USEBACKQ TOKENS=*" %%F IN ("%FileList%") DO XCOPY /F /Y "%Source%\%%~F" "%Destination%\%%~F"
+
+GOTO :EOF
